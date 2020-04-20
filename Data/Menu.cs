a822f8826ae7b63566525c14904030711d659271
erj2016 <erj2016@ksu.edu>
2020-04-19 19:46:28 -0500

    Completed menu class, index page, and updated error page

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -4,10 +4,90 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class Menu
+    /// <summary>
+    /// This class implements methods to create list of menu items
+    /// </summary>
+    public class Menu
     {
-        public IEnumerable<IOrderItem> Entrees { get; set; }
+        /// <summary>
+        /// Compiles list of entrees
+        /// </summary>
+        public IEnumerable<IOrderItem> Entrees() 
+        {
+            return new List<IOrderItem>()
+            {
+            new AngryChicken(),
+            new CowpokeChili(),
+            new DakotaDoubleBurger(),
+            new PecosPulledPork(),
+            new RustlersRibs(),
+            new TexasTripleBurger(),
+            new TrailBurger()
 
-        List<IOrderItem> = new List<IOrderItem> {CowboyCoffee() };
+            };
+        }
+
+        /// <summary>
+        /// Compiles list of drinks
+        /// </summary>
+        /// <returns>List of drinks</returns>
+        public IEnumerable<IOrderItem> Drinks()
+        {
+            return new List<IOrderItem>()
+            {
+                new CowboyCoffee(),
+                new JerkedSoda(),
+                new TexasTea(),
+                new Water()
+            };
+        }
+
+
+        /// <summary>
+        /// Compiles list of sides
+        /// </summary>
+        /// <returns>List of sides</returns>
+        public IEnumerable<IOrderItem> Sides()
+        {
+            return new List<IOrderItem>()
+            {
+                new BakedBeans(),
+                new ChiliCheeseFries(),
+                new CornDodgers(),
+                new PanDeCampo()
+            };
+        }
+
+        /// <summary>
+        /// Complete list of menu items
+        /// </summary>
+        /// <returns>List of menu items</returns>
+        public IEnumerable<IOrderItem> CompleteMenu()
+        {
+            List<IOrderItem> completeMenu = new List<IOrderItem>();
+
+            IEnumerable<IOrderItem> drink = Drinks();
+            IEnumerable<IOrderItem> entree = Entrees();
+            IEnumerable<IOrderItem> side = Sides();
+            foreach (Drink d in drink)
+            {
+                completeMenu.Add(d);
+            }
+            foreach (Entree e in entree)
+            {
+                completeMenu.Add(e);
+            }
+            foreach (Side s in side)
+            {
+                completeMenu.Add(s);
+            }
+            return completeMenu;
+
+        }
+
+
+
+       
+
     }
 }
